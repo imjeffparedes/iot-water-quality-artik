558286d9ada8db9715c849b5bd8f5858f20c540f
nonedeactivated <jeffpar0721@outlook.com>
2017-03-17 17:11:02 +0800

    Added thread for reading sensors.

--- software.ino
+++ software.ino
@@ -1,3 +1,7 @@
+#include <StaticThreadController.h>
+#include <Thread.h>
+#include <ThreadController.h>
+
 /********************
 * Here's my Code for Water Quality Device Monitoring
 * This Arduino code will collect ph and temp data from MKR1000 
@@ -8,7 +12,6 @@
 *
 ********************/
 
-
 #include <WiFi101.h>
 #include <WiFiClient.h>
 #include <ArduinoJson.h> 
@@ -17,34 +20,54 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-/**Temperature Sensor Initialization**/
+
+// Thread where reading sensors run
+Thread myThread = Thread();
+
+
+/*
+ * Temperature Sensor Initialization
+ * 
+ */
+ 
+// we'll save celsius readings here
+static float celsius=0;
 #define ONE_WIRE_BUS 1                // Data wire is plugged into digital port 1 of Arduino
 OneWire oneWire(ONE_WIRE_BUS);        // Setup a oneWire instance to communicate with any OneWire 
                                       //devices (not just Maxim/Dallas temperature ICs)
 DallasTemperature sensors(&oneWire);  // Pass our oneWire reference to Dallas Temperature. 
 
-/** ARTIK Cloud REST Initialization **/
+/** 
+ *  ARTIK Cloud REST Initialization
+**/
 char server[] = "api.artik.cloud";    // Samsung ARTIK Cloud API Host
 int port = 443;                       // 443 for HTTPS 
-
 char buf[200];                        // body data to store the JSON to be sent to the ARTIK cloud 
-
 String deviceID = "artik cloud device id"; // put your device id here created from tutorial 
 String deviceToken = "artik cloud device token"; // put your device token here created from tutorial
+int sendInterval = 5;                 // send time interval in seconds
 
-/**pH meter initialization**/
+/**
+ * pH meter initialization
+**/
 #define SensorPin A1                  // pH meter Analog output to Arduino Analog Input 1
 #define Offset 0.00                   // deviation compensate
-#define samplingInterval 20
+#define samplingInterval 800
 #define ArrayLenth  40                // times of collection
 int pHArray[ArrayLenth];              // Store the average value of the sensor feedback
 int pHArrayIndex=0;   
 
-int status = -1;
-int millis_start;
-int sendInterval = 5;                 // send time interval in seconds
+// global variables
+// we'll save readings here
+static float  pHValue, voltage;
 
-/**Wifi Setting**/
+// status of wifi connection
+int status = -1;
+
+
+/**
+ * Wifi Setting
+**/
 #define WIFI_AP "your wifi ssid"
 #define WIFI_PWD "wifi password"
 
@@ -55,33 +78,23 @@ HttpClient client = HttpClient(wifi, server, port);
 
 void setup(void) {
   pinMode(13, OUTPUT);           // set pin 13 (LED) to output
-  millis_start = millis();
   Serial.begin(9600);
+
+  // set up reading sensor thread
+  myThread.onRun(getReadings);
+  myThread.setInterval(500);
+  
   startWifi();                             //start connecting to wifi
 }
 
 void loop(void) {
-    /*Aquiring current temperature*/
-   float celsius = 0;
-   sensors.requestTemperatures();          // Send the command to get temperatures
-   celsius = sensors.getTempCByIndex(0);
-   sensors.requestTemperatures();          // Send the command to get temperatures
-   celsius = sensors.getTempCByIndex(0);
 
-
-  /*Aquiring current pH value*/
-  static unsigned long samplingTime = millis();
-  static unsigned long printTime = millis();
-  static float pHValue,voltage;
   
-  if(millis()-samplingTime > samplingInterval)
-  {
-      pHArray[pHArrayIndex++]=analogRead(SensorPin);
-      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
-      voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
-      pHValue = 3.5*voltage+Offset;
-      samplingTime=millis();
-  }
+  // checks if thread should run
+  if(myThread.shouldRun())
+    myThread.run();
+    
+   
   
   Serial.println("==========================================="); 
   Serial.println("We will send these json data"); 
@@ -92,8 +105,6 @@ void loop(void) {
   Serial.print(" , temp: ");
   Serial.print(celsius);
   Serial.println("} ");
-
-  Serial.println("");
   
   Serial.println("Start sending data"); 
   String contentType = "application/json"; 
@@ -117,6 +128,10 @@ void loop(void) {
     digitalWrite(13, HIGH);       // turn on LED
     delay(500);                  // Make delay fro blink
     digitalWrite(13, LOW);       // turn on LED
+    Serial.print("Successfully sent data."); 
+  }else{
+    
+    Serial.print("Failed sending data."); 
   }
   Serial.print("Status code: "); 
   Serial.println(statusCode); 
@@ -126,7 +141,38 @@ void loop(void) {
   
   
 }
-/*Init Connection to Wifi*/
+
+/*
+ * Here we read the sensors
+ */
+
+
+ void getReadings(){
+   // Aquiring current temperature
+   sensors.requestTemperatures();          // Send the command to get temperatures
+   celsius = sensors.getTempCByIndex(0);
+   sensors.requestTemperatures();          // Send the command to get temperatures
+   celsius = sensors.getTempCByIndex(0);
+
+
+   // Aquiring current pH value
+  static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  
+  if(millis()-samplingTime > samplingInterval)
+  {
+      pHArray[pHArrayIndex++]=analogRead(SensorPin);
+      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+      voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
+      pHValue = 3.5*voltage+Offset;
+      samplingTime=millis();
+  }
+  
+ }
+
+/*
+ * Connect to wifi settings
+*/
 void startWifi(){
   Serial.println("Connecting MKR1000 to network...");
   //  WiFi.begin();
@@ -142,7 +188,10 @@ void startWifi(){
 }
 
 
-/*DfRobot pH Meter Source*/
+/*
+ * Voltage convertion and averaging for ph electrode
+ * From DfRobot pH Meter wifi
+*/
 double avergearray(int* arr, int number){
   int i;
   int max,min;
@@ -183,7 +232,9 @@ double avergearray(int* arr, int number){
   return avg;
 }
 
-/*Buffer to send on REST*/
+/*
+ * Buffer to send on API
+*/
 int loadBuffer(float temp, float ph ) {   
   StaticJsonBuffer<200> jsonBuffer; // reserve spot in memory 
   JsonObject& root = jsonBuffer.createObject(); // create root objects 
